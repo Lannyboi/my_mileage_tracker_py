58738baf6dd65d7098a44691c15ae00570550046
lannyboi33 <graytech33@gmail.com>
2023-11-01 16:40:32 -0700

    another postgres try

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session, jsonify
-from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
@@ -12,7 +13,6 @@ app.jinja_env.filters["usd"] = usd
 app.config['SECRET_KEY'] = 'the friends we made along the way'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://lvyoumqzneedyl:21cd8831f240f60715dfaf722f3508b97bbcd7d3205836cbe371ceec2fbde3c3@ec2-3-230-24-12.compute-1.amazonaws.com:5432/dbvf8fu77agh8h"
 Session(app)
 
