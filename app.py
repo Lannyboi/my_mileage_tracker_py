d3828e3dfc6e094b4b513b00fec9727023b4f60c
lannyboi33 <graytech33@gmail.com>
2023-11-01 14:35:30 -0700

    changed data base from local to Postgress

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///mileageTracker.db")
+db = SQL("postgres://tqrrveabnjnggh:3ead9b25df2ab62e326dd018e3fa880a838b11ccbcfc3e06ac33627cb9d1a529@ec2-44-206-204-65.compute-1.amazonaws.com:5432/d9mbs9feas8dm1")
 
 """ app routes """
 @app.route("/")
